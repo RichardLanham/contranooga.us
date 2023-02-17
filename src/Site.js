e5c261f5fd880875a37f5d8ce84999984f9be37b
Richard Lanham <richard@RichardM1ProMac.local>
2023-02-17 17:45:49 -0500

    put side image back

--- src/Site.js
+++ src/Site.js
@@ -4,9 +4,9 @@ import { Helmet, HelmetProvider } from "react-helmet-async";
 import SiteTopBar from "./components/SiteTopBar";
 import useMediaQuery from "@mui/material/useMediaQuery";
 import { eventEmitter } from "./events";
-// import { IconButton } from "@mui/material";
+import { IconButton } from "@mui/material";
 import StrapiPages from "./components/StrapiPages";
-// import ArrowUpwardIcon from "@mui/icons-material/ArrowUpward";
+import ArrowUpwardIcon from "@mui/icons-material/ArrowUpward";
 import CssBaseline from "@mui/material/CssBaseline";
 import { StyledSideBar } from "./styles/ComponentStyles";
 import Banners from "./components/Banners";
@@ -71,6 +71,92 @@ const Site = (props) => {
 
   if (!theme.global) return null;
 
+  const StyledSiteDiv = styled("div")(({ theme }) => ({
+    // backgroundColor: theme.palette.grey[200],
+    // [theme.breakpoints.down("xl")]: {},
+    // [theme.breakpoints.down("lg")]: {},
+    // [theme.breakpoints.down("md")]: {},
+    // [theme.breakpoints.down("sm")]: {},
+  }));
+
+  const StyledSiteName = styled("div")(({ theme }) => ({
+    ...theme.typography.h3,
+    // fontSize: "3.5vw",
+    // margin: "auto",
+    position: "absolute",
+    top: 20,
+    // left: "calc(28%)",
+    width: "calc(90% -1px)",
+    // paddingLeft: 10,
+    // paddingRight: 10,
+    marginTop: 40,
+    borderRadius: 5,
+    padding: 3,
+    // border: "2px solid",
+    // borderColor: theme.palette.primary.main,
+    boxShadow: theme.shadows[10],
+    // whiteSpace: "wrap",
+    // justifyContent: "center",
+    //  backgroundColor: theme.palette.background.paper,
+    // color: theme.palette.primary.contrastText,
+    [theme.breakpoints.down("lg")]: {
+      // left: "calc(15%)",
+      // top: 0,
+      // fontSize: 30,
+      // left: 50,
+    },
+    [theme.breakpoints.down("md")]: {
+      ...theme.typography.h5,
+      // width: 300,
+      // left: 8,
+    },
+    [theme.breakpoints.down("sm")]: {
+      // ...theme.typography.h5,
+      left: 20,
+      // width: 300,
+      // left: 8,
+    },
+  }));
+
+  const StyledHeading = styled("div")(({ theme }) => ({
+    ...theme.typography.h2,
+
+    top: 50,
+    width: "100%",
+    margin: "auto",
+    [theme.breakpoints.down("lg")]: {
+      ...theme.typography.h3,
+    },
+    [theme.breakpoints.down("md")]: {
+      ...theme.typography.h4,
+    },
+    [theme.breakpoints.down("sm")]: {
+      ...theme.typography.h5,
+    },
+  }));
+
+  const bgImage = require("./assets/bgimages/collage_float_left1.png");
+  const StyledFloatImg = styled("div")(({ theme }) => ({
+    backgroundImage: `url(${bgImage})`,
+    // backgroundSize: "cover",
+    backgroundPosition: "center",
+    zIndex: 0,
+    position: "fixed",
+    top: 0,
+    // bottom: 0,
+    left: 0,
+    width: "14vw",
+    borderRadius: 15,
+    // width: 400,
+    height: 1000,
+
+    [theme.breakpoints.down("lg")]: {
+      opacity: ".35",
+    },
+    [theme.breakpoints.down("md")]: {},
+    [theme.breakpoints.down("sm")]: {},
+  }));
+
   return (
     <HelmetProvider title={title}>
       <CssBaseline />
@@ -84,8 +170,9 @@ const Site = (props) => {
         {<meta name="theme-color" content={theme.palette.primary.main} />}
         <link rel="canonical" href="http://contranooga.us/" />
       </Helmet>
-
-      <div>
+      {/* <StyledSiteName>{theme.global.metadata.metaDescription}</StyledSiteName> */}
+      <StyledFloatImg>HELLO;</StyledFloatImg>
+      <StyledSiteDiv>
         {user && (
           <div
             style={{
@@ -106,13 +193,14 @@ const Site = (props) => {
         <StyledSideBar>
           <StrapiPages position="side" />
         </StyledSideBar>
-        <div name="PageWrap" style={{ position: "relative" }}>
+        <div style={{ position: "relative" }}>
           <Login />
+
           <Banners />
 
           {props.children}
         </div>
-      </div>
+      </StyledSiteDiv>
     </HelmetProvider>
   );
 };
