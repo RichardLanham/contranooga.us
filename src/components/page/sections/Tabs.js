1b995b01fcde149b780e88f15fd9f9bbf253e620
Richard Lanham <richard@RichardM1ProMac.local>
2023-03-04 12:37:51 -0500

    TABS

--- src/components/page/sections/Tabs.js
+++ src/components/page/sections/Tabs.js
@@ -12,23 +12,66 @@ const Tabs = ({ section }) => {
   const theme = useTheme();
   const [slug, setSlug] = useState("");
 
-  const showTab = (e) => {
-    console.log(e.currentTarget.name);
-    e.preventDefault();
-    setSlug(e.currentTarget.name);
+  const [_tab, setTab] = useState("home");
+
+  const showTab = (event) => {
+    console.log(event.currentTarget.name);
+    event.preventDefault();
+    setTab(event.currentTarget.name);
   };
   const tabSectionThumb = getThumb(section?.picture?.data?.attributes);
 
-  console.log(section.tabs.tab.map((t) => t.slug));
+  // console.log(section.tabs.tab.map((t) => t.slug));
 
   const TabHeader = () => {
     // console.log(tabSectionThumb);
     return (
       <div>
-        <img src={process.env.REACT_APP_STRAPI + tabSectionThumb.url} />
-        <div>{section.label}</div>
-        <div>{section.description}</div>
-        <div>{section.title}</div>
+        <div
+          style={{
+            ...theme.flexRows,
+            gap: 5,
+          }}
+        >
+          {tabSectionThumb ? (
+            <img src={process.env.REACT_APP_STRAPI + tabSectionThumb.url} />
+          ) : null}
+          {section.title && (
+            <div style={{ ...theme.typography.h5, marginTop: "auto" }}>
+              {section.title}
+            </div>
+          )}
+          {section.description && (
+            <div
+              style={{
+                ...theme.typography.body1,
+                // ...theme.typography.fontWeightBold,
+                // fontWeight: 600,
+                fontStyle: "italic",
+                marginBottom: "auto",
+              }}
+            >
+              {section.description}
+            </div>
+          )}
+        </div>
+        <div
+          style={{
+            ...theme.flexRows,
+            gap: 5,
+          }}
+        >
+          {section?.tabs.title && (
+            <div style={{ ...theme.typography.h6, marginTop: "auto" }}>
+              {section.tabs.title}
+            </div>
+          )}
+          {section?.tabs.description && (
+            <div style={{ ...theme.typography.caption, marginTop: "auto" }}>
+              {section.tabs.description}
+            </div>
+          )}
+        </div>
       </div>
     );
   };
@@ -38,26 +81,54 @@ const Tabs = ({ section }) => {
     if (section?.section?.tabs?.type === "link") {
       return (
         <a
-          onClick={(e) => e.preventDefault()}
-          name="HomeTabButton"
+          name="home"
+          onClick={showTab}
           style={{
             ...theme.typography.h5,
+
             textDecoration: "none",
-            backgroundColor: theme.palette.background.default,
-            padding: 3,
-            borderRadius: 15,
-            borderBottom: 20,
-            border: "1px solid blue",
+            color:
+              "home" === _tab
+                ? theme.palette.primary.contrastDark
+                : theme.palette.primary.dark,
+            backgroundColor:
+              "home" === _tab
+                ? theme.palette.primary.dark
+                : theme.palette.primary.contrastDark,
+            display: "block",
+            marginBottom: 10,
+            width: "fit-content",
+            // color: "green",
           }}
           href="#"
         >
-          {section.section.tabs.title}
+          {section.section.label}
         </a>
       );
     }
 
     if (section?.section?.tabs?.type === "button") {
-      return <Button variant="outline">{section?.section?.tabs?.title}</Button>;
+      return (
+        <Button
+          name="home"
+          onClick={showTab}
+          style={{
+            backgroundColor: theme.palette.primary.contrastDark,
+            marginBottom: 10,
+            color:
+              "home" === _tab
+                ? theme.palette.primary.contrastDark
+                : theme.palette.primary.dark,
+            backgroundColor:
+              "home" === _tab
+                ? theme.palette.primary.dark
+                : theme.palette.primary.contrastDark,
+          }}
+          variant="outline"
+        >
+          {section?.section?.label}
+        </Button>
+      );
     }
     if (section?.section?.tabs?.type === "image") {
       return (
@@ -72,9 +143,9 @@ const Tabs = ({ section }) => {
     return <div>nothing to make</div>;
   };
 
-  const TabContent = ({ inputs }) => {
+  const TabContent = ({ slug, inputs }) => {
     return (
-      <div>
+      <div style={{ display: _tab === slug ? "block" : "none" }}>
         {inputs.map((input, key) => {
           return (
             <div key={key}>
@@ -116,7 +187,17 @@ const Tabs = ({ section }) => {
       <div name="tabButtonWrap">
         {tab.type === "button" && (
           <Button
-            name={tab.label}
+            style={{
+              color:
+                tab.slug === _tab
+                  ? theme.palette.primary.contrastDark
+                  : theme.palette.primary.dark,
+              backgroundColor:
+                tab.slug === _tab
+                  ? theme.palette.primary.dark
+                  : theme.palette.primary.contrastDark,
+            }}
+            name={tab.slug}
             onClick={showTab}
             id={tab.slug}
             //onClick={(e) => handleTabClick()}
@@ -128,22 +209,33 @@ const Tabs = ({ section }) => {
 
         {tab.type === "link" && (
           <a
-            onClick={(e) => e.preventDefault()}
-            name="HomeTabButton"
+            onClick={showTab}
+            name={tab.slug}
             style={{
               ...theme.typography.h5,
               textDecoration: "none",
-              backgroundColor: theme.palette.background.default,
-              padding: 3,
-              borderRadius: 15,
-              borderBottom: 20,
-              border: "1px solid blue",
+              width: "fit-content",
+              // backgroundColor: theme.palette.background.default,
+              // padding: 3,
+              // borderRadius: 15,
+              // borderBottom: 20,
+              // border: "1px solid blue",
             }}
             href="#"
           >
             {tab.label}
           </a>
         )}
+      </div>
+    );
+  };
+
+  const TabButtons = ({ tabs }) => {
+    return (
+      <div style={{ ...theme.flexRows, gap: 5 }}>
+        {tabs.tab.map((tab, key) => {
+          return <TabButton key={key} tab={tab} />;
+        })}
       </div>
     );
   };
@@ -154,17 +246,17 @@ const Tabs = ({ section }) => {
     <div>
       <TabHeader section={section} />
       <TabHomeTabButton section={section} />
+      <TabButtons tabs={section.tabs} />
       <div>
         {section.tabs.tab.map((tab, key) => {
           return (
             <div key={key}>
-              <TabButton tab={tab} />
-              <TabContent inputs={tab.inputs} />
+              <TabContent slug={tab.slug} inputs={tab.inputs} />
             </div>
           );
         })}
         <div>
-          <TabContent inputs={section.inputs} />
+          <TabContent slug="home" inputs={section.inputs} />
         </div>
       </div>
     </div>
