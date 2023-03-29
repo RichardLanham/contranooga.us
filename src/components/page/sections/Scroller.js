502f066c44c6877685bccf0e72f8557eb50b0197
Richard Lanham <richard@RichardM1ProMac.local>
2023-03-29 16:07:08 -0400

    Scroller add StyledHeader(slug) to separate, consistency

--- src/components/page/sections/Scroller.js
+++ src/components/page/sections/Scroller.js
@@ -89,12 +89,39 @@ export const Scroller = ({ section }) => {
     setSlug(e.target.name);
   };
 
+  const StyledHeader = styled("div")(({ theme }) => ({
+    ...theme.typography.h3,
+    // opacity: ".1",
+    width: "calc(50% - 1px)",
+    paddingLeft: 10,
+    paddingRight: 10,
+    marginTop: 20,
+    marginLeft: 10,
+    borderRadius: 5,
+    borderColor: theme.palette.primary.main,
+    boxShadow: theme.shadows[10],
+    [theme.breakpoints.down("xl")]: {
+      marginTop: 15,
+    },
+    [theme.breakpoints.down("lg")]: {
+      left: 50,
+    },
+    [theme.breakpoints.down("md")]: {
+      fontSize: 22,
+      left: 8,
+      marginLeft: 0,
+    },
+    [theme.breakpoints.down("sm")]: {
+      marginTop: 15,
+    },
+  }));
   return (
     <StyledPageSection
       style={{
         border: "1px none red",
       }}
     >
+      <StyledHeader>{slug}</StyledHeader>
       <Button
         style={{ display: "none" }}
         name={section.slugs[0].slug}
