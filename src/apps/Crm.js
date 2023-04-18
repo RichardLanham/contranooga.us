38bada037c5f39c5f5dbffc58abde3a56ee24d45
Richard Lanham <richard@RichardM1ProMac.local>
2023-04-18 19:19:11 -0400

    crm msg change2

--- src/apps/Crm.js
+++ src/apps/Crm.js
@@ -175,9 +175,9 @@ const Crm = () => {
   };
 
   const Controls = () => {
-    const [info, setInfo] = useState("The caller is Vicki Herndon");
+    const [info, setInfo] = useState("The caller is Sarah Melton");
     const [day, setDay] = useState("April 22nd");
-    theme.infoDay = "April 8th";
+    theme.infoDay = "April 2nd";
     theme.infoMessage = "The caller is Sarah Melton";
     const handleInfo = (e) => {
       console.log(e.target.value);
